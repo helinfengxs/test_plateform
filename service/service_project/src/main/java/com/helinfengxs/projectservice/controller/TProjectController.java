1efbfbc272f96603461b0106c82a59c55db09bc6
helinfeng <helinfengxs@163.com>
2021-05-07 16:08:43 +0800

    配置分页插件以及测试分页数据查询

--- service/service_project/src/main/java/com/helinfengxs/projectservice/controller/TProjectController.java
+++ service/service_project/src/main/java/com/helinfengxs/projectservice/controller/TProjectController.java
@@ -1,6 +1,8 @@
 package com.helinfengxs.projectservice.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.helinfengxs.commonutils.R;
 import com.helinfengxs.projectservice.entity.TProject;
 import com.helinfengxs.projectservice.service.TProjectService;
@@ -10,6 +12,7 @@ import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -39,6 +42,11 @@ public class TProjectController {
         return R.ok().data("items",list);
     }
 
+    /**
+     * 根据项目id删除项目接口
+     * @param id 项目id
+     * @return 返回删除是否成功
+     */
     @DeleteMapping("{id}")
     @ApiOperation(value = "根据项目Id删除项目")
     public R removeId(@ApiParam(name = "id",value = "项目管理id",required = true) @PathVariable String id){
@@ -48,5 +56,31 @@ public class TProjectController {
         }
         return R.ok();
     }
-}
+
+    /**
+     * 分页查询所有项目信息
+     * @param current 当前页
+     * @param limit 当前页展示数
+     * @return 查询后的分页数据
+     */
+    @ApiOperation(value = "分页cha")
+    @GetMapping("pageProject/{current}/{limit}")
+    public R pageListProject(
+            @ApiParam(name = "current",value = "当前页",required = true)
+            @PathVariable long current,
+            @ApiParam(name = "limit",value = "当前页面展示数",required = true)
+            @PathVariable long limit
+    ){
+
+        Page<TProject> page = new Page<>(current,limit);
+        IPage<TProject> listProject = tProjectService.page(page, null);
+        long total = listProject.getTotal();
+        long cut = listProject.getCurrent();
+        long pages = listProject.getPages();
+        List<TProject> records = listProject.getRecords();
+
+        return R.ok().data("total",total).data("current",cut).data("pages",pages).data("rows",records);
+
+
+    }}
 
