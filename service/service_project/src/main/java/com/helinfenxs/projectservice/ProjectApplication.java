42032cb66f3ec4e847d36b4c3c8a3b83a0543b98
helinfeng <helinfengxs@163.com>
2021-05-07 10:53:44 +0800

    整合Swagger

--- service/service_project/src/main/java/com/helinfenxs/projectservice/ProjectApplication.java
+++ service/service_project/src/main/java/com/helinfenxs/projectservice/ProjectApplication.java
@@ -2,8 +2,10 @@ package com.helinfenxs.projectservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.helinfengxs"})
 public class ProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class,args);
