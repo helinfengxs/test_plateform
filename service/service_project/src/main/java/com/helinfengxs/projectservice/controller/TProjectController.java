f56cfc8524e439533cc1a21156eaed9e4bf4aad1
helinfeng <helinfengxs@163.com>
2021-05-07 15:23:51 +0800

    统一返回结果以及测试统一返回结果

--- service/service_project/src/main/java/com/helinfengxs/projectservice/controller/TProjectController.java
+++ service/service_project/src/main/java/com/helinfengxs/projectservice/controller/TProjectController.java
@@ -1,6 +1,7 @@
 package com.helinfengxs.projectservice.controller;
 
 
+import com.helinfengxs.commonutils.R;
 import com.helinfengxs.projectservice.entity.TProject;
 import com.helinfengxs.projectservice.service.TProjectService;
 import io.swagger.annotations.Api;
@@ -27,17 +28,25 @@ public class TProjectController {
     @Autowired
     private TProjectService tProjectService;
 
+    /**
+     * 查询所有项目信息
+     * @return 返回项目列表信息
+     */
     @GetMapping("findAll")
     @ApiOperation(value = "查询所有项目列表")
-    public List<TProject> findAll(){
+    public R findAll(){
         List<TProject> list = tProjectService.list(null);
-        return list;
+        return R.ok().data("items",list);
     }
+
     @DeleteMapping("{id}")
     @ApiOperation(value = "根据项目Id删除项目")
-    public boolean removeId(@ApiParam(name = "id",value = "项目管理id",required = true) @PathVariable String id){
-        boolean status = tProjectService.removeById(id);
-        return status;
+    public R removeId(@ApiParam(name = "id",value = "项目管理id",required = true) @PathVariable String id){
+        boolean flag = tProjectService.removeById(id);
+        if (!flag){
+            return R.error();
+        }
+        return R.ok();
     }
 }
 
